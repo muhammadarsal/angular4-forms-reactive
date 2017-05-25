ba9c2cd1fa2b82f6bfd43f37a2f42b6a5320de76
Muhammad Arsal Abdullah <arsal@plerosys.com>
2017-05-25 16:25:53 +0500

    9 - formarray

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+
+/*
+	For reactive approach to work we need
+	to import ReactiveFormsModule in app.module
+*/
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   genders = ['male', 'female'];
+  signupForm: FormGroup;
+
+  ngOnInit() {
+  	this.signupForm = new FormGroup({
+  		// defining the controls for this form
+
+  		/* 
+  			wrapping keys in quotes to make sure that during
+				minification, this property name is kept. Though
+				this might not be necessary
+			*/
+
+			/*
+				we can have form groups in FormGroup and also	need 
+				reflect this in our markup using formGroupName directive 
+			*/
+			'userdata': new FormGroup({
+				// default value null
+	  		'username': new FormControl(null, Validators.required),
+	  		'email'		: new FormControl(null, [Validators.required, Validators.email]) 
+			}),
+
+			// default value 'male'
+  		'gender' : new FormControl('male'),
+  		'hobbies': new FormArray([])
+  	})
+  }
+
+  onSubmit() {
+  	console.log(this.signupForm)
+  }
+
+  onAddHobby() {
+  	const control = new FormControl(null, Validators.required);
+  	(<FormArray>this.signupForm.get('hobbies')).push(control);
+  }
 }
